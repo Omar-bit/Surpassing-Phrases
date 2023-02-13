a593f77e1e34c402c1706ba68c6cff5690ab742b
Omar-bit <Bouassidaomar9@gmail.com>
2023-02-13 20:38:58 +0100

    loading code

---
+++ Surpassing Phrases.py
@@ -0,0 +1,12 @@
+from math import *
+def is_surpassing_phrase(ch):
+    t=ch.split()
+    for i in range(len(t)):
+        diff,prev=0,0
+        for j in range(len(t[i])-1):
+            diff =abs(ord(t[i][j])-ord(t[i][j+1]))
+            if diff<prev:
+                return False
+            prev=diff
+    return True
+print(is_surpassing_phrase("")).
